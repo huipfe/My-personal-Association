3b47c2578e57a0b89fd34d527955104bc8b032af
Ilan Tervil <ilan.tervil@wanadoo.fr>
2023-02-07 18:58:44 +0100

    23e commit advance in js button change mod parameter

--- script.js
+++ script.js
@@ -58,9 +58,9 @@ document.querySelectorAll(".ChangeThemeDiv").forEach(div => {
     div.addEventListener("click", event => { /* Quand tu cliques sur ma div*/
         let themeTarget = event.currentTarget.dataset.theme;
         if (themeTarget == "spring") {  /*Si ThemeTarget = success*/
-            document.querySelectorAll(".bg-0").forEach(warningElement => {
-                warningElement.classList.remove("bg-0");
-                warningElement.classList.add("bg-0-alt");
+            document.querySelectorAll(".bg-0").forEach(autumnElement => {
+                autumnElement.classList.remove("bg-0");
+                autumnElement.classList.add("bg-0-alt");
             });
             //récupère tout les élements qui ont la classe bg-0
             //bg-0 -> bg-0-alt
@@ -69,52 +69,81 @@ document.querySelectorAll(".ChangeThemeDiv").forEach(div => {
             /* Dans ce cas là, on applique le thème spring */
 
             //faire la même chose, mais pour le texte
-            document.querySelectorAll(".text-bg-0-alt").forEach(warningElement => {
-                warningElement.classList.remove("text-bg-0-alt");
-                warningElement.classList.add("text-bg-0");
+            document.querySelectorAll(".text-bg-0-alt").forEach(autumnElement => {
+                autumnElement.classList.remove("text-bg-0-alt");
+                autumnElement.classList.add("text-bg-0");
             });
 
-            document.querySelectorAll(".text-bg-0").forEach(successElement => {
-                successElement.classList.remove("text-bg-0");
-                successElement.classList.add("text-bg-0-alt");
+            document.querySelectorAll(".text-bg-0").forEach(springElement => {
+                springElement.classList.remove("text-bg-0");
+                springElement.classList.add("text-bg-0-alt");
             });
 
             //faire la même chose, pour la navbar Header bg-0 
-            document.querySelectorAll(".navbar-bg-0").forEach(warningElement => {
-                warningElement.classList.remove("navbar-bg-0");
-                warningElement.classList.add("navbar-bg-0-alt");
+            document.querySelectorAll(".navbar-bg-0").forEach(autumnElement => {
+                autumnElement.classList.remove("navbar-bg-0");
+                autumnElement.classList.add("navbar-bg-0-alt");
             });
 
             //faire la même chose, pour le bouton, Section Contact 3 bg-3
-            document.querySelectorAll(".btn-bg-3").forEach(warningElement => {
-                warningElement.classList.remove("btn-bg-3");
-                warningElement.classList.add("btn-bg-3-alt");
+            document.querySelectorAll(".btn-bg-3").forEach(autumnElement => {
+                autumnElement.classList.remove("btn-bg-3");
+                autumnElement.classList.add("btn-bg-3-alt");
             });
 
             //faire la même chose, pour la section 0 bg-1 Présentation et histoire,
             //de l'association & section 1 bg-1 Résumer et objectif de l'association
-            document.querySelectorAll(".bg-1").forEach(darkElement => {
-                darkElement.classList.remove("bg-1");
-                darkElement.classList.add("bg-1-alt");
+            document.querySelectorAll(".bg-1").forEach(autumnElement => {
+                autumnElement.classList.remove("bg-1");
+                autumnElement.classList.add("bg-1-alt");
             });
 
             //faire la même chose, pour la section 2 bg-2 Gallerie d'image
-            document.querySelectorAll(".bg-2").forEach(darkElement => {
-                darkElement.classList.remove("bg-2");
-                darkElement.classList.add("bg-2-alt");
+            document.querySelectorAll(".bg-2").forEach(autumnElement => {
+                autumnElement.classList.remove("bg-2");
+                autumnElement.classList.add("bg-2-alt");
             });
 
             //faire la même chose, pour la footer 2 bg-4
-            document.querySelectorAll(".bg-4").forEach(darkElement => {
-                darkElement.classList.remove("bg-4");
-                darkElement.classList.add("bg-4-alt");
+            document.querySelectorAll(".bg-4").forEach(autumnElement => {
+                autumnElement.classList.remove("bg-4");
+                autumnElement.classList.add("bg-4-alt");
             });
 
             //faire la même chose, pour la modal bg-modal-1
-            document.querySelectorAll(".bg-modal-1").forEach(darkElement => {
-                darkElement.classList.remove("bg-modal-1");
-                darkElement.classList.add("bg-modal-1-alt");
+            document.querySelectorAll(".bg-modal-1").forEach(autumnElement => {
+                autumnElement.classList.remove("bg-modal-1");
+                autumnElement.classList.add("bg-modal-1-alt");
             });
+
+            //faire la même chose, pour le picto spring Logo theme1
+            document.querySelectorAll(".picto-theme1").forEach(imageSpring => {
+                imageSpring.src = "image/patte de chien picto alt spring.png";
+            });
+            // alternative
+            // const imageSpring = document.querySelector(".picto-theme1");
+            // imageSpring.src = "image/patte de chien picto alt spring.png";
+
+
+            //faire la même chose, pour le picto spring button navbar theme2
+            // document.querySelectorAll(".picto-theme1").forEach(imageSpring => {
+            //     imageSpring.src = "image/patte de chien picto alt spring.png";
+            // });
+
+            //faire la même chose, pour le picto spring button parametre theme3
+            // document.querySelectorAll(".picto-theme1").forEach(imageSpring => {
+            //     imageSpring.src = "image/patte de chien picto alt spring.png";
+            // });
+
+            //faire la même chose, pour le picto spring button scrolltop theme5
+            // document.querySelectorAll(".picto-theme1").forEach(imageSpring => {
+            //     imageSpring.src = "image/patte de chien picto alt spring.png";
+            // });
+
+            //faire la même chose, pour le picto spring button scrollDown theme4
+            // document.querySelectorAll(".picto-theme1").forEach(imageSpring => {
+            //     imageSpring.src = "image/patte de chien picto alt spring.png";
+            // });
         }
 
         //autumn = Chaud, thème Autonme/dark/no alt/bg-0,1,2,3,4/warning
@@ -122,9 +151,9 @@ document.querySelectorAll(".ChangeThemeDiv").forEach(div => {
 
         /* Partie automne, car site devenu printemps durant la partie précédente */
         else if (themeTarget == "autumn") {  /*Si ThemeTarget = dark*/
-            document.querySelectorAll(".bg-0-alt").forEach(lightElement => {
-                lightElement.classList.remove("bg-0-alt");
-                lightElement.classList.add("bg-0");
+            document.querySelectorAll(".bg-0-alt").forEach(springElement => {
+                springElement.classList.remove("bg-0-alt");
+                springElement.classList.add("bg-0");
             })
             //récupère tout les élements qui ont la classe bg-0-alt
             //bg-0-alt -> bg-0
@@ -132,14 +161,14 @@ document.querySelectorAll(".ChangeThemeDiv").forEach(div => {
             /* Dans ce cas là, on applique le thème autumn */
 
             //faire la même chose, mais pour le texte
-            document.querySelectorAll(".text-bg-0").forEach(warningElement => {
-                warningElement.classList.remove("text-bg-0");
-                warningElement.classList.add("text-bg-0-alt");
+            document.querySelectorAll(".text-bg-0").forEach(autumnElement => {
+                autumnElement.classList.remove("text-bg-0");
+                autumnElement.classList.add("text-bg-0-alt");
             });
 
-            document.querySelectorAll(".text-bg-0-alt").forEach(successElement => {
-                successElement.classList.remove("text-bg-0-alt");
-                successElement.classList.add("text-bg-0");
+            document.querySelectorAll(".text-bg-0-alt").forEach(springElement => {
+                springElement.classList.remove("text-bg-0-alt");
+                springElement.classList.add("text-bg-0");
             });
 
             //faire la même chose, pour la navbar Header bg-0-alt
@@ -149,35 +178,63 @@ document.querySelectorAll(".ChangeThemeDiv").forEach(div => {
             });
 
             //faire la même chose, pour le bouton, Section Contact 3 bg-3-alt
-            document.querySelectorAll(".btn-bg-3-alt").forEach(warningElement => {
-                warningElement.classList.remove("btn-bg-3-alt");
-                warningElement.classList.add("btn-bg-3");
+            document.querySelectorAll(".btn-bg-3-alt").forEach(autumnElement => {
+                autumnElement.classList.remove("btn-bg-3-alt");
+                autumnElement.classList.add("btn-bg-3");
             });
 
             //faire la même chose, pour la section 0 bg-1-alt Présentation et histoire,
             //de l'association & section 1 bg-1-alt Résumer et objectif de l'association
-            document.querySelectorAll(".bg-1-alt").forEach(darkElement => {
-                darkElement.classList.remove("bg-1-alt");
-                darkElement.classList.add("bg-1");
+            document.querySelectorAll(".bg-1-alt").forEach(autumnElement => {
+                autumnElement.classList.remove("bg-1-alt");
+                autumnElement.classList.add("bg-1");
             });
 
             //faire la même chose, pour la section 2 bg-2-alt Gallerie d'image
-            document.querySelectorAll(".bg-2-alt").forEach(darkElement => {
-                darkElement.classList.remove("bg-2-alt");
-                darkElement.classList.add("bg-2");
+            document.querySelectorAll(".bg-2-alt").forEach(autumnElement => {
+                autumnElement.classList.remove("bg-2-alt");
+                autumnElement.classList.add("bg-2");
             });
 
             //faire la même chose, pour la footer 2 bg-4-alt
-            document.querySelectorAll(".bg-4-alt").forEach(darkElement => {
-                darkElement.classList.remove("bg-4-alt");
-                darkElement.classList.add("bg-4");
+            document.querySelectorAll(".bg-4-alt").forEach(autumnElement => {
+                autumnElement.classList.remove("bg-4-alt");
+                autumnElement.classList.add("bg-4");
             });
 
             //faire la même chose, pour la modal bg-modal-1-alt
-            document.querySelectorAll(".bg-modal-1-alt").forEach(darkElement => {
-                darkElement.classList.remove("bg-modal-1-alt");
-                darkElement.classList.add("bg-modal-1");
+            document.querySelectorAll(".bg-modal-1-alt").forEach(autumnElement => {
+                autumnElement.classList.remove("bg-modal-1-alt");
+                autumnElement.classList.add("bg-modal-1");
             });
+
+            //faire la même chose, pour le picto autumn Logo theme1
+            document.querySelectorAll(".picto-theme1").forEach(imageSpring => {
+                imageSpring.src = "image/patte de chien picto2.png";
+            });
+            //alternative
+            // const imageSpring = document.querySelector(".picto-theme1");
+            // imageSpring.src = "image/patte de chien picto2.png";
+
+            //faire la même chose, pour le picto spring button navbar theme2
+            // document.querySelectorAll(".picto-theme1").forEach(imageSpring => {
+            //     imageSpring.src = "image/patte de chien picto alt spring.png";
+            // });
+
+            //faire la même chose, pour le picto spring button parametre theme3
+            // document.querySelectorAll(".picto-theme1").forEach(imageSpring => {
+            //     imageSpring.src = "image/patte de chien picto alt spring.png";
+            // });
+
+            //faire la même chose, pour le picto spring button scrolltop theme5
+            // document.querySelectorAll(".picto-theme1").forEach(imageSpring => {
+            //     imageSpring.src = "image/patte de chien picto alt spring.png";
+            // });
+
+            //faire la même chose, pour le picto spring button scrollDown theme4
+            // document.querySelectorAll(".picto-theme1").forEach(imageSpring => {
+            //     imageSpring.src = "image/patte de chien picto alt spring.png";
+            // });
         }
 
     });
