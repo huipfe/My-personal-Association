338e6d25bb20225732cc3a1ed5543037842ad495
Ilan Tervil <ilan.tervil@wanadoo.fr>
2023-02-07 17:03:27 +0100

    22e commit finish change theme parameter js functionnality (autumn mod)

--- script.js
+++ script.js
@@ -48,7 +48,7 @@ document.addEventListener("scroll", (event) => {
 });
 
 
-/** 3. success/warning Mode : 
+/** 3. spring/autumn Mode : 
  * changement de thème dans la page, thème Autonme ou Printemps */
 //autumn = Chaud, thème Autonme/dark/no alt/bg-0,1,2,3,4/warning
 //spring = froid, thème Printemps/light/alt/bg-0,1,2,3,4-alt/sucess
@@ -62,10 +62,11 @@ document.querySelectorAll(".ChangeThemeDiv").forEach(div => {
                 warningElement.classList.remove("bg-0");
                 warningElement.classList.add("bg-0-alt");
             });
-            //récupère tout les élements qui ont la classe bg-warning
-            //bg-warning -> bg-success
-            //pour chaque élement dark element, on enlève, pour remplacer par success element.
-            /* Dans ce cas là, on applique le thème success */
+            //récupère tout les élements qui ont la classe bg-0
+            //bg-0 -> bg-0-alt
+            //pour chaque élement autumn element, on enlève, pour
+            //remplacer par spring element.
+            /* Dans ce cas là, on applique le thème spring */
 
             //faire la même chose, mais pour le texte
             document.querySelectorAll(".text-bg-0-alt").forEach(warningElement => {
@@ -115,44 +116,67 @@ document.querySelectorAll(".ChangeThemeDiv").forEach(div => {
                 darkElement.classList.add("bg-modal-1-alt");
             });
         }
-        /* Partie Sombre, car site devenu clair durant la partie précédente */
+
+        //autumn = Chaud, thème Autonme/dark/no alt/bg-0,1,2,3,4/warning
+        //spring = froid, thème Printemps/light/alt/bg-0,1,2,3,4-alt/sucess
+
+        /* Partie automne, car site devenu printemps durant la partie précédente */
         else if (themeTarget == "autumn") {  /*Si ThemeTarget = dark*/
-            document.querySelectorAll(".bg-light").forEach(lightElement => {
-                lightElement.classList.remove("bg-light");
-                lightElement.classList.add("bg-dark");
+            document.querySelectorAll(".bg-0-alt").forEach(lightElement => {
+                lightElement.classList.remove("bg-0-alt");
+                lightElement.classList.add("bg-0");
             })
-            //récupère tout les élements qui ont la classe bg-light
-            //bg-light -> bg-dark
-            //pour chaque élement light element, on enlève, pour remplacer par dark element.
-            /* Dans ce cas là, on applique le thème dark */
+            //récupère tout les élements qui ont la classe bg-0-alt
+            //bg-0-alt -> bg-0
+            //pour chaque élement spring element, on enlève, pour remplacer par autumn element.
+            /* Dans ce cas là, on applique le thème autumn */
 
             //faire la même chose, mais pour le texte
-            document.querySelectorAll(".text-dark").forEach(lightElement => {
-                lightElement.classList.remove("text-dark");
-                lightElement.classList.add("text-light");
+            document.querySelectorAll(".text-bg-0").forEach(warningElement => {
+                warningElement.classList.remove("text-bg-0");
+                warningElement.classList.add("text-bg-0-alt");
             });
 
-            document.querySelectorAll(".text-bg-light").forEach(lightElement => {
-                lightElement.classList.remove("text-bg-light");
-                lightElement.classList.add("text-bg-dark");
+            document.querySelectorAll(".text-bg-0-alt").forEach(successElement => {
+                successElement.classList.remove("text-bg-0-alt");
+                successElement.classList.add("text-bg-0");
             });
 
-            //faire la même chose, pour la navbar 
-            document.querySelectorAll(".navbar-light").forEach(lightElement => {
-                lightElement.classList.remove("navbar-light");
-                lightElement.classList.add("navbar-dark");
+            //faire la même chose, pour la navbar Header bg-0-alt
+            document.querySelectorAll(".navbar-bg-0-alt").forEach(warningElement => {
+                warningElement.classList.remove("navbar-bg-0-alt");
+                warningElement.classList.add("navbar-bg-0");
             });
 
-            //faire la même chose, pour le bouton 
-            document.querySelectorAll(".btn-light").forEach(lightElement => {
-                lightElement.classList.remove("btn-light");
-                lightElement.classList.add("btn-dark");
+            //faire la même chose, pour le bouton, Section Contact 3 bg-3-alt
+            document.querySelectorAll(".btn-bg-3-alt").forEach(warningElement => {
+                warningElement.classList.remove("btn-bg-3-alt");
+                warningElement.classList.add("btn-bg-3");
             });
 
-            //faire la même chose, pour le corp des cards (card-body)
-            document.querySelectorAll(".light").forEach(lightElement => {
-                lightElement.classList.remove("light");
-                lightElement.classList.add("dark");
+            //faire la même chose, pour la section 0 bg-1-alt Présentation et histoire,
+            //de l'association & section 1 bg-1-alt Résumer et objectif de l'association
+            document.querySelectorAll(".bg-1-alt").forEach(darkElement => {
+                darkElement.classList.remove("bg-1-alt");
+                darkElement.classList.add("bg-1");
+            });
+
+            //faire la même chose, pour la section 2 bg-2-alt Gallerie d'image
+            document.querySelectorAll(".bg-2-alt").forEach(darkElement => {
+                darkElement.classList.remove("bg-2-alt");
+                darkElement.classList.add("bg-2");
+            });
+
+            //faire la même chose, pour la footer 2 bg-4-alt
+            document.querySelectorAll(".bg-4-alt").forEach(darkElement => {
+                darkElement.classList.remove("bg-4-alt");
+                darkElement.classList.add("bg-4");
+            });
+
+            //faire la même chose, pour la modal bg-modal-1-alt
+            document.querySelectorAll(".bg-modal-1-alt").forEach(darkElement => {
+                darkElement.classList.remove("bg-modal-1-alt");
+                darkElement.classList.add("bg-modal-1");
             });
         }
 
